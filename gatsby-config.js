8a21db6f81a01fa16f5da5dbce7c7305266af9b5
jondubman <jon@dubman.com>
2020-02-18 15:25:35 -0800

    Under construction

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Future Blog`,
+    author: `Jonathan Dubman`,
+    description: `Blog TBD`,
+    siteUrl: `https://www.dubman.com`,
     social: {
-      twitter: `kylemathews`,
     },
   },
   plugins: [
